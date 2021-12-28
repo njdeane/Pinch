c2381874958123656dd795213dedd51876d6d0bc
Nic Deane <njdeane7@gmail.com>
2021-12-28 16:31:16 +1000

    Add drag gesture.

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,8 +12,15 @@ struct ContentView: View {
   // MARK: - Properties
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero
   
   // MARK: - Functions
+  private func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
   
   // MARK: - Content
   
@@ -28,6 +35,7 @@ struct ContentView: View {
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
           .animation(.linear(duration: 1), value: isAnimating)
           .scaleEffect(imageScale)
           .onTapGesture(count: 2) {
@@ -37,10 +45,27 @@ struct ContentView: View {
               }
             } else {
               withAnimation(.spring()) {
-                imageScale = 1
+                resetImageState()
               }
             }
           }
+          .gesture(
+            DragGesture()
+              .onChanged { value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
+                }
+              }
+              .onEnded { _ in
+                if imageScale <= 1 {
+                  withAnimation(.spring()) {
+                    resetImageState()
+                  }
+                } else {
+                  
+                }
+              }
+          )
       } //: ZStack
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)

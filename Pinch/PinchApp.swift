c41b160b51c70197f6599a86612fc24c5d81d748
Nic Deane <njdeane7@gmail.com>
2021-12-28 15:15:16 +1000

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Nic Deane on 28/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
